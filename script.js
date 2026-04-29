d0a6632c4c3b47eac8daa82ee8fd11611477160b
alejandroE345 <alejandroencinas345@gmail.com>
2026-04-29 18:48:35 +0000

    progress

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 console.log("Script Started");
 
-// 
+// Function to allow the user to log in to their account
+function userInput(event) {
+    console.log("click");
+    event.preventDefault();
+    let emailInput = document.getElementById("emailaddress");
+    let passInput = document.getElementById("password");
+
+   let email = emailInput.value;
+   let pwd = passInput.value;
+
+   console.log(email);
+   console.log(pwd);
+   
+   alert("Logged in! Redirecting...")
+   redirectUser();
+}
+
+// Function to redirect the user to the app after logging in
+function redirectUser() {
+    console.log("click");
+    //Checks is the user's login info matches
+    if (email == emailInput.value && pwd == passInput.value) {
+        window.location.href = "index.html";
+    } else {
+        alert("Email or Password is incorrect!");
+    }
+    let redirectButton = document.getElementById("login");
+    redirectButton.addEventListener("click", redirectUser);
+}
